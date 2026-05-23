59e42fb382275bd3ef2b760fff5458c92f215a09
Gustavo <gustavobaister@gmail.com>
2026-05-22 21:23:55 -0300

    Criando model, atribuições e Enum

---
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Enums/GeneroFilme.java
@@ -0,0 +1,17 @@
+package com.Senai.Filmes.Model.Enums;
+
+public enum GeneroFilme {
+    ACAO,
+    COMEDIA,
+    DRAMA,
+    TERROR,
+    ROMANCE,
+    FICCAO_CIENTIFICA,
+    ANIMACACAO,
+    DOCUMENTARIO,
+    SUSPENSE,
+    ESPORTE,
+    CULT,
+    AVENTURA,
+    MUSICAL
+}
---
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Filme.java
@@ -0,0 +1,39 @@
+package com.Senai.Filmes.Model;
+
+
+import com.Senai.Filmes.Model.Enums.GeneroFilme;
+import jakarta.persistence.*;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "Filmes")
+public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "O título é obrigatório")
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private String urlPoster;
+
+    @NotNull(message = "O campo genero é obrigatório!")
+    @Enumerated(EnumType.STRING)
+    private GeneroFilme genero;
+
+    @NotNull(message = "")
+    @Min(value = 1, message = "A duração deve ser maior que 0")
+    private Integer duracaoMinutos;
+
+}
